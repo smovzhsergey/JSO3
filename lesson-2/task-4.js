45c0d5466529499d953ecfd48504acd1259083be
smovzhsergey <smovzhsergey@gmail.com>
2019-06-24 11:47:51 +0300

    task 4-6

---
+++ lesson-2/task-4.js
@@ -0,0 +1,20 @@
+const value = 8;
+
+switch (value) {
+    case 0:
+        console.log(0);
+        break;
+    
+    case 1:
+        console.log(1);
+        break;
+
+    case 2:
+
+    case 3:
+        console.log('2,3');
+        break;
+    
+    default:
+        break;  
+}
---
+++ lesson-2/task-6.js
@@ -0,0 +1,8 @@
+const array = [1, 2, 3, 4];
+
+let result = 0;
+
+for (item of array) {
+    result = result + item;
+}
+
